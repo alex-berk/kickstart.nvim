02f21f4a090b63f4d274a82abc066bf89fa388aa
Alex Berk <aleks.berkov@gmail.com>
2024-01-13 15:52:32 +0200

    plugins setup

--- init.lua
+++ init.lua
@@ -44,11 +44,13 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.o.scrolloff = 8
+
 local map = vim.api.nvim_set_keymap
-map('n', '<Leader>o', 'o<Escape>', {noremap = true, silent = false})
-map('n', '<Leader>O', 'O<Escape>', {noremap = true, silent = false})
-map('n', '<c-d>', '<c-d>zz', {noremap = true, silent = false})
-map('n', '<c-u>', '<c-u>zz', {noremap = true, silent = false})
+map('n', '<Leader>o', 'o<Escape>', { noremap = true, silent = false })
+map('n', '<Leader>O', 'O<Escape>', { noremap = true, silent = false })
+map('n', '<c-d>', '<c-d>zz', { noremap = true, silent = false })
+map('n', '<c-u>', '<c-u>zz', { noremap = true, silent = false })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
@@ -94,7 +96,8 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      -- TODO: find and disable auto pretiier
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -119,7 +122,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -264,7 +267,7 @@ require('lazy').setup({
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
   -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -272,7 +275,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
